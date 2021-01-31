3b2a217d521975a3203e898dcd78e9e09acd3f81
Uchechukwu Darlington Emmanuel <daleentontech@gmail.com>
2021-01-31 22:54:33 +0100

    define model and test case

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import
+from .models import Post
 # Create your tests here.
+
+
+class BlogTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #Create a User
+        testuser1 = User.objects.create_user(
+            username='testuser1',
+            password='abc123'
+        )
+        testuser1.save()
+
+        #create a blog post
+        test_post = Post.objects.create(
+            author=testuser1,
+            title='Blog title',
+            body='Blog Content...'
+        )
+        test_post.save()
